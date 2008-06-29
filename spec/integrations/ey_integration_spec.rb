d9b52acf1d8ca49b16a1dc04ee082119f7a175e4
atmos <atmos@atmos.org>
2008-06-29 13:56:51 -0600

    small integration spec to make this finish in under 20 minute :(

--- spec/integrations/ey_integration_spec.rb
+++ spec/integrations/ey_integration_spec.rb
@@ -2,22 +2,33 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 if ENV['DO_EM_ALL']
   describe XenConfigFile::Parser, " simple_parsing " do
-    before(:all) do
-      @parser = XenConfigFile::Parser.new
-    end
-  
-    Dir["#{ENV['EY_FILES'] || File.expand_path(File.dirname(__FILE__)+'/../../../scrapetacular')}/xen/*/*/*.xen"].each do |input_file|
-      # next unless input_file =~ /(ey01-s00429|ey01-s00294|ey00-s00124|ey00-s00309|ey00-s00166|ey00-s00112|ey00-s00074)/
-      # describe "#{File.basename(input_file)} as input" do
+    before(:all) { @parser = XenConfigFile::Parser.new }
+    
+    Dir["#{ENV['EY_FILES'] || File.expand_path(File.dirname(__FILE__)+'/../../../scrapetacular')}/xen/*/*/*.xen"].each_with_index do |input_file, idx|
       describe "#{input_file} as input" do
-        before(:all) do
-          @result = @parser.simple_parse(File.read(input_file))
+        before(:all) { @result = @parser.simple_parse(File.read(input_file)) }
+        if(idx % 25)
+          after(:all) { GC.enable }
+        else
+          after(:all) { GC.disable }
         end
+
         it "shouldn't be nil" do
           @result.should_not be_nil
         end
         it "should return an AST instance of the config file" do
           @result.should be_a_kind_of(XenConfigFile::AST::ConfigFile)          
+        end
+
+        describe "generation " do
+          before(:all) do
+            @visitor = XenConfigFile::AST::Visitor::PrettyPrintVisitor.new
+            @reparser = @parser.simple_parse(@result.accept(@visitor))
+          end
+          
+          it "should generate matching content" do
+            @reparser.accept(@visitor).should == @result.accept(@visitor)
+          end
         end
       end
     end
